95e0e7ef90b5cfe6f8cafc0a3da3c0fbe1d63e68
GDM <jinwook31@naver.com>
2016-07-13 16:55:35 +0900

    LiFi

---
+++ module_code(Manchester)/LiFi_receive/LiFi_receive.ino
@@ -0,0 +1,92 @@
+#define MYID 13
+
+//Pin
+int relay = 10;
+int light_sensor = A0;
+
+//Sensor Var
+int curr_light;  int prev_light;
+int data[5];
+int data_size = 0;
+
+//Start bit
+boolean start;
+
+//Decode signal
+int ID;
+int msg;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relay, OUTPUT);
+  Serial.begin(9600);
+  start = false;
+  prev_light = analogRead(light_sensor);
+  delay(10);
+  curr_light = analogRead(light_sensor);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(start == false){  //get start bit
+    if(is_changed() == true){
+      start = true;
+      delay(10);
+    }
+  }else{  //if start bit is true -> get Data
+    if(data_size == 5){  //finish getting signal
+      decode_data();
+      data_size = 0;
+      start = false;
+      delay(10);
+    }else{ //reading signal
+      read_data();
+      OnOff();
+    }
+  }
+  print_val();
+}
+
+void print_val(){
+  for(int i = 0; i < 5; i++){
+    Serial.print(data[i]);
+  }Serial.println();
+}
+
+void read_data(){    //Get Binary code from signal (chagne : 1 / no change : 0) [Manchester Encoding]
+  if(is_changed() == true){
+    data[data_size] = 1;
+  }else{
+    data[data_size] = 0;
+    delay(250);                         //term!!!!  0.25s
+  }
+  data_size++;
+}
+
+boolean is_changed(){    //check if the binary signal have chaged
+  if(abs(curr_light - prev_light) > 500){                            //need to change if enviroment changed!!
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void decode_data(){
+  int j = 1;
+  for(int i = 3; i>-1; i--){
+    ID += data[i] * j;
+    j *= 2;
+  }
+  msg = data[4];
+}
+
+void OnOff(){
+  if(ID == MYID){
+    if(msg == 1){
+      digitalWrite(relay, HIGH);
+    }else{
+      digitalWrite(relay, LOW);
+    }
+  }
+}
+
---
+++ module_code(Manchester)/LiFi_send/LiFi_send.ino
@@ -0,0 +1,31 @@
+int LED = 13;
+int signals[5] = {1,1,0,1,1};
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(LED, OUTPUT);
+  
+  //get ID + msg from DB <in Raspberry pi>  13 / 1
+
+  //convert to Binary  11011
+
+  //LED 초기 상태 = LOW(OFF)
+
+  //send
+  digitalWrite(LED, HIGH);  //start bit
+  delay(10);
+  
+  digitalWrite(LED, LOW);  //1
+  
+  digitalWrite(LED, HIGH);  //1
+
+  delay(250);  //0
+
+  digitalWrite(LED, LOW);   //1
+
+  digitalWrite(LED, HIGH);  //1
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
